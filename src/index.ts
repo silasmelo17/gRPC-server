901ad43fca0fd1798d2f3e3f9cdfa7bf2b5b84f1
silas melo <silasmelo1967@gmail.com>
2023-09-03 20:05:26 -0300

    adding dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 
 import 'reflect-metadata';
 
+import path from 'path';
+import dotenv from 'dotenv';
+dotenv.config({ path: path.resolve(__dirname, './.env') });
+
 import grpc from 'grpc';
 import { getContainer } from './container';
 
 (async () => {
-    const container = getContainer();
+    const container = await getContainer();
 
     const server = container.get(grpc.Server);
-    server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
+    server.bind(`0.0.0.0:${process.env.GRPC_PORT}`, grpc.ServerCredentials.createInsecure());
     server.start();
 })();
